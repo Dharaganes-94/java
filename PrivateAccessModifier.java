fc9dfc866ec8d9420f8a833aa0204bc1090bae07
dharani <dharani.gsd@gmail.com>
2023-09-20 22:00:21 +0530

    Added new files

---
+++ PrivateAccessModifier.java
@@ -0,0 +1,33 @@
+package intermediate;
+
+class A
+{
+	private int i=10;
+	int a=15;
+	private void msg()
+	{
+		System.out.println("Private method");
+	}
+	
+	void msg1()
+	{
+		System.out.println("Not a private method");
+	}
+	
+}
+
+public class PrivateAccessModifier {
+	
+	public static void main(String args[])
+	{
+		A obj= new A();
+		obj.msg1();
+		System.out.println(obj.a);
+		//compile time error
+		//obj.msg();
+		
+	}
+	
+
+}
+
---
+++ PublicAccessModifier.java
@@ -0,0 +1,20 @@
+package intermediate;
+
+import Basics.ReturnTypeEx;
+
+public class PublicAccessModifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ReturnTypeEx obj= new ReturnTypeEx();
+		long factResult= obj.factorialCalc(15);
+		
+		System.out.println("The factorial result is -->"+factResult);
+
+		
+
+
+	}
+
+}
